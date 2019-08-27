7d24cd04e0a7c721ccaf4f768753a69551f8e4ae
Hector Valdez <hvaldez180@gmail.com>
2019-08-27 00:10:38 -0400

    created a temp data.js file

---
+++ 30before30/src/data.js
@@ -0,0 +1,31 @@
+const tempCategoriesList = [
+    {
+        id:0,
+        name: "Travel",
+    },
+
+    {
+        id:1,
+        name: "Fitness",
+    },
+
+    {
+        id: 2,
+        name: "Work",
+    },
+
+    {
+        id: 3,
+        name: "Learning",
+    },
+
+    {
+        id: 4,
+        name: "Love",
+    },
+
+    {
+        id: 5,
+        name: "Unspecified",
+    },
+]
