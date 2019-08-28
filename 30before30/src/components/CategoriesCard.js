7f8072a93863c1efe4f7dae836e43181e158a53d
Hector Valdez <hvaldez180@gmail.com>
2019-08-28 18:35:18 -0400

    routing categories card

--- 30before30/src/components/CategoriesCard.js
+++ 30before30/src/components/CategoriesCard.js
@@ -3,47 +3,48 @@ import { Card } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 
 export default function CategoriesCard() {
+
   return (
     <div className="category-list">
       <Card
         className="card"
         id="travel"
-        href="#card-example-link-card"
+        href="/travel"
         header="Travel"
       />
 
       <Card
         className="card"
         id="health-and-fitness"
-        href="#card-example-link-card"
+        href="/health-and-fitness"
         header="Health and Fitness"
       />
 
       <Card
         className="card"
         id="work"
-        href="#card-example-link-card"
+        href="/work"
         header="Work"
       />
 
       <Card
         className="card"
         id="learning"
-        href="#card-example-link-card"
+        href="/learning"
         header="Learning"
       />
 
       <Card
         className="card"
         id="love"
-        href="#card-example-link-card"
+        href="/learning"
         header="Love"
       />
 
       <Card
         className="card"
         id="unspecified"
-        href="#card-example-link-card"
+        href="/unspecified"
         header="Unspecified"
       />
     </div>
