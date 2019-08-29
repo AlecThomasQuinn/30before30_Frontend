449daeedf3b1722bf05432a9caab16af636d7fec
Hector Valdez <hvaldez180@gmail.com>
2019-08-29 17:07:24 -0400

    cleaned up some code in catergoriesList component

--- 30before30/src/components/CategoriesList.js
+++ 30before30/src/components/CategoriesList.js
@@ -4,18 +4,34 @@ import axios from 'axios';
 
 export default function CategoriesList() {
 
-    const [data, setData] = useState([]);
-    useEffect(() => {
-      axios.get(`https:/thirty-before-thirty-bw.herokuapp.com//api/user-items`)
-      .then(response => {
-        setData(response);
-        console.log(response);
-
-      });
-
-    }, []);
-
     return (
         <CategoriesCard />
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const [data, setData] = useState([]);
+    // useEffect(() => {
+    //   axios.get(`https:/thirty-before-thirty-bw.herokuapp.com//api/user-items`)
+    //   .then(response => {
+    //     setData(response);
+    //     console.log(response);
+
+    //   });
+
+    // }, []);
