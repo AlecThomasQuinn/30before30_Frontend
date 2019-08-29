0dfc8ada401347737a8ac4aa8aa108f4da32d326
Hector Valdez <hvaldez180@gmail.com>
2019-08-29 12:28:46 -0400

    added the img and title to the work component

--- 30before30/src/components/WorkComponent.js
+++ 30before30/src/components/WorkComponent.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function WorkComponent () {
     return (
         <div>
-            <img />
+            <h1 className="work-h1">Work</h1>
+            <img className="work-img" src="https://images.unsplash.com/photo-1556745753-b2904692b3cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80" alt="a picture of work"/>
         </div>
     )
 }
