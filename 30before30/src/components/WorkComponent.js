37665c4ad778d7e78be8e3dd98bd89cce9f98127
Hector Valdez <hvaldez180@gmail.com>
2019-08-29 23:21:47 -0400

    added styles to work component

--- 30before30/src/components/WorkComponent.js
+++ 30before30/src/components/WorkComponent.js
@@ -10,7 +10,7 @@ const WorkComponent = () =>  (
             const bucketlist = context;
             console.log(bucketlist,"testing");
                 return (
-        <div>
+        <div className="work-container">
             <h1 className="work-h1 animated bounce delay-2s">Work</h1>
             <img className="work-img" src="https://images.unsplash.com/photo-1556745753-b2904692b3cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80" alt="picture of health and fitness"/>
             {bucketlist.bucketList.map(item => {
