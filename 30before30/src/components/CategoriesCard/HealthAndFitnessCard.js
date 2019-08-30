4812facc2461061793477c8a27bdba65738a6cf8
Hector Valdez <hvaldez180@gmail.com>
2019-08-30 11:57:30 -0400

    added styling to categories card

--- 30before30/src/components/CategoriesCard/HealthAndFitnessCard.js
+++ 30before30/src/components/CategoriesCard/HealthAndFitnessCard.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Card } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 const HealthAndFitnessCard = (item) => {
     console.log(item, "is this an item")
         return (
             <>
-                <div className ='bucketListItem'>
-                    <div className='itemDetails' style={{borderBottom:"1px solid black"}}>
-                        <h3>{item.item.item_name}</h3>
-                        <p>{item.item.description}</p>
-                        <p>{item.item.target_date}</p>
-                    </div>
+                <div className="health-and-fitness-card">
+                    <Card>
+                        <div className ='bucketListItem'>
+                            <div className='itemDetails' style={{borderBottom:"1px solid black"}}>
+                                <h3>{item.item.item_name}</h3>
+                                <p>{item.item.description}</p>
+                                <p>{item.item.target_date}</p>
+                            </div>
+                        </div>
+                    </Card>
                 </div>
             </>
-        )
+                )
 };
 
 export default HealthAndFitnessCard;
--- 30before30/src/components/CategoriesCard/LearningCard.js
+++ 30before30/src/components/CategoriesCard/LearningCard.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Card } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 const LearningCard = (item) => {
     console.log(item, "is this an item")
         return (
             <>
+            <div className="learing-card">
+            <Card className="card">
                 <div className ='bucketListItem'>
                     <div className='itemDetails' style={{borderBottom:"1px solid black"}}>
                         <h3>{item.item.item_name}</h3>
@@ -11,6 +15,8 @@ const LearningCard = (item) => {
                         <p>{item.item.target_date}</p>
                     </div>
                 </div>
+            </Card>
+            </div>
             </>
         )
 };
