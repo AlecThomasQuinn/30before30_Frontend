0ab0f33cb3aecff0a2c509f8cd9dfff16de7ea00
Hector Valdez <hvaldez180@gmail.com>
2019-08-29 22:47:18 -0400

    added LoveCard to LoveComponent

--- 30before30/src/components/LoveComponent.js
+++ 30before30/src/components/LoveComponent.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { AppState } from "../contexts/AppContext";
+import LoveCard from "./CategoriesCard/LoveCard";
 
-export default function LoveComponent () {
-    return (
+
+const LoveComponent = () =>  (
+
+    
+
+        <AppState.Consumer>{(context)=>{
+            const bucketlist = context;
+            console.log(bucketlist,"testing");
+                return (
         <div>
             <h1 className="love-h1 animated bounce delay-2s">Love</h1>
-            <img className="love-img" src="https://images.unsplash.com/photo-1474552226712-ac0f0961a954?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80" alt="a picture of love"/>
+            <img className="love-img" src="https://images.unsplash.com/photo-1553531889-3836a7ee6d3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80" alt="picture of health and fitness"/>
+            {bucketlist.bucketList.map(item => {
+                if (item.category_name === "Love"){
+                    return <LoveCard item={item} key={item.id}/>;
+
+                }
+            
+            })}
         </div>
-    )
-}
+                )
+            }} 
+
+        </AppState.Consumer>
+)
+
+export default LoveComponent
