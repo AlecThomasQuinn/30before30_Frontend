ce0468d03750a0f25fb3367b3c757ce3b98002b9
Hector Valdez <hvaldez180@gmail.com>
2019-08-29 22:00:33 -0400

    debugged work component title, also added a new file learningCard and added it to learning component

--- 30before30/src/components/LearningComponent.js
+++ 30before30/src/components/LearningComponent.js
@@ -1,10 +1,31 @@
 import React from "react";
+import { AppState } from "../contexts/AppContext";
+import LearningCard from "./CategoriesCard/LearningCard";
 
-export default function LearningComponent () {
-    return (
+
+const LearningComponent = () =>  (
+
+    
+
+        <AppState.Consumer>{(context)=>{
+            const bucketlist = context;
+            console.log(bucketlist,"testing");
+                return (
         <div>
             <h1 className="learning-h1 animated bounce delay-2s">Learning</h1>
-            <img className="learning-img" src="https://www.jisc.ac.uk/sites/default/files/styles/710px-wide/public/library-laptop-user.jpg?itok=XjSjwmvx" alt="a picture of learning"/>
+            <img className="learning-img" src="https://www.jisc.ac.uk/sites/default/files/styles/710px-wide/public/library-laptop-user.jpg?itok=XjSjwmvx" alt="picture of health and fitness"/>
+            {bucketlist.bucketList.map(item => {
+                if (item.category_name === "Learning"){
+                    return <LearningCard item={item} key={item.id}/>;
+
+                }
+            
+            })}
         </div>
-    )
-}
+                )
+            }} 
+
+        </AppState.Consumer>
+)
+
+export default LearningComponent
